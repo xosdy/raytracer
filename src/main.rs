f4ba1b572fbcdd71a20180fdd13b033542a9f2b2
Tang King Fai <xosdy.t@gmail.com>
2019-04-10 14:30:08 +0800

    Add more spheres

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,18 @@ use std::fs::File;
 use std::io::{BufWriter, Write};
 
 fn main() -> Result<(), Box<std::error::Error>> {
-    let s = Sphere::new(na::Vector3::new(-3., 0., -16.), 2.);
-    render(&s)?;
+    let mut spheres = vec![];
+    spheres.push(Sphere::new(na::Vector3::new(-3., 0., -16.), 2.));
+    spheres.push(Sphere::new(na::Vector3::new(-1., -1.5, -12.), 2.));
+    spheres.push(Sphere::new(na::Vector3::new(1.5, -0.5, -18.), 3.));
+    spheres.push(Sphere::new(na::Vector3::new(7., 5., -18.), 4.));
+
+    render(&spheres)?;
 
     Ok(())
 }
 
-fn render(sphere: &Sphere) -> std::io::Result<()> {
+fn render(spheres: &Vec<Sphere>) -> std::io::Result<()> {
     let width = 1024;
     let height = 768;
     let fov = std::f32::consts::PI / 2.;
@@ -25,7 +30,7 @@ fn render(sphere: &Sphere) -> std::io::Result<()> {
                 / height as f32;
             let y2 = -(2. * (y as f32 + 0.5) / height as f32 - 1.) * (fov / 2.).tan();
             let dir = na::Vector3::new(x2, y2, -1.).normalize();
-            framebuffer.push(cast_ray(&na::Vector3::zeros(), &dir, &sphere));
+            framebuffer.push(cast_ray(&na::Vector3::zeros(), &dir, &spheres));
         }
     }
 
@@ -45,10 +50,23 @@ fn render(sphere: &Sphere) -> std::io::Result<()> {
 fn cast_ray(
     origin: &na::Vector3<f32>,
     direction: &na::Vector3<f32>,
-    sphere: &Sphere,
+    spheres: &Vec<Sphere>,
 ) -> na::Vector3<f32> {
-    sphere
-        .ray_intersect(origin, direction)
-        .map(|_| na::Vector3::new(0.4, 0.4, 0.3))
-        .unwrap_or_else(|| na::Vector3::new(0.2, 0.7, 0.8))
+    if scene_intersect(origin, direction, spheres) {
+        na::Vector3::new(0.4, 0.4, 0.3)
+    } else {
+        na::Vector3::new(0.2, 0.7, 0.8)
+    }
+}
+
+fn scene_intersect(
+    origin: &na::Vector3<f32>,
+    direction: &na::Vector3<f32>,
+    spheres: &Vec<Sphere>,
+) -> bool {
+    spheres
+        .iter()
+        .filter_map(|s| s.ray_intersect(origin, direction))
+        .min_by(|x, y| x.partial_cmp(y).unwrap_or(std::cmp::Ordering::Equal))
+        .is_some()
 }
