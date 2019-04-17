9cb672397e39bc808ebc2c53c12c1970135a1ae3
Tang King Fai <xosdy.t@gmail.com>
2019-04-17 16:17:09 +0800

    Add refraction

--- src/main.rs
+++ src/main.rs
@@ -12,31 +12,50 @@ use crate::material::Material;
 use crate::sphere::Sphere;
 
 struct RaycastHit {
-    material: Material,
-    position: na::Vector3<f32>,
-    normal: na::Vector3<f32>,
+    pub material: Material,
+    pub position: na::Vector3<f32>,
+    pub normal: na::Vector3<f32>,
+}
+
+impl RaycastHit {
+    pub fn get_surface(&self, dir: &na::Vector3<f32>) -> na::Vector3<f32> {
+        if dir.dot(&self.normal) < 0. {
+            self.position - self.normal * 1e-3
+        } else {
+            self.position + self.normal * 1e-3
+        }
+    }
 }
 
 fn main() -> Result<(), Box<std::error::Error>> {
     let ivory = Material::new(
-        na::Vector3::new(0.6, 0.3, 0.1),
+        na::Vector4::new(0.6, 0.3, 0.1, 0.),
         na::Vector3::new(0.4, 0.4, 0.3),
         50.,
+        1.,
+    );
+    let glass = Material::new(
+        na::Vector4::new(0., 0.5, 0.1, 0.8),
+        na::Vector3::new(0.6, 0.7, 0.8),
+        125.,
+        1.5,
     );
     let red_rubber = Material::new(
-        na::Vector3::new(0.9, 0.1, 0.),
+        na::Vector4::new(0.9, 0.1, 0., 0.),
         na::Vector3::new(0.3, 0.1, 0.1),
         10.,
+        1.,
     );
     let mirror = Material::new(
-        na::Vector3::new(0., 10., 0.8),
+        na::Vector4::new(0., 10., 0.8, 0.),
         na::Vector3::new(1., 1., 1.),
         1425.,
+        1.,
     );
 
     let spheres = vec![
         Sphere::new(na::Vector3::new(-3., 0., -16.), 2., ivory),
-        Sphere::new(na::Vector3::new(-1., -1.5, -12.), 2., mirror),
+        Sphere::new(na::Vector3::new(-1., -1.5, -12.), 2., glass),
         Sphere::new(na::Vector3::new(1.5, -0.5, -18.), 3., red_rubber),
         Sphere::new(na::Vector3::new(7., 5., -18.), 4., mirror),
     ];
@@ -105,12 +124,11 @@ fn cast_ray(
 
     if let Some(hit) = scene_intersect(origin, direction, spheres) {
         let reflect_dir = -reflect(direction, &hit.normal).normalize();
-        let reflect_origin = if reflect_dir.dot(&hit.normal) < 0. {
-            hit.position - hit.normal * 1e-3
-        } else {
-            hit.position + hit.normal * 1e-3
-        };
+        let refract_dir = refract(direction, hit.normal, hit.material.refractive_index).normalize();
+        let reflect_origin = hit.get_surface(&reflect_dir);
+        let refract_origin = hit.get_surface(&refract_dir);
         let reflect_color = cast_ray(&reflect_origin, &reflect_dir, spheres, lights, depth - 1);
+        let refract_color = cast_ray(&refract_origin, &refract_dir, spheres, lights, depth - 1);
 
         let mut diffuse_light_intensity = 0.;
         let mut specular_light_intensity = 0.;
@@ -118,11 +136,7 @@ fn cast_ray(
             let light_dir = (light.position - hit.position).normalize();
             let light_dist = (light.position - hit.position).norm();
 
-            let shadow_origin = if light_dir.dot(&hit.normal) < 0. {
-                hit.position - hit.normal * 1e-3
-            } else {
-                hit.position + hit.normal * 1e-3
-            };
+            let shadow_origin = hit.get_surface(&light_dir);
 
             if let Some(shadow_hit) = scene_intersect(&shadow_origin, &light_dir, spheres) {
                 if (shadow_hit.position - shadow_origin).norm() < light_dist {
@@ -141,6 +155,7 @@ fn cast_ray(
         hit.material.diffuse_color * diffuse_light_intensity * hit.material.albedo[0]
             + na::Vector3::new(1., 1., 1.) * specular_light_intensity * hit.material.albedo[1]
             + reflect_color * hit.material.albedo[2]
+            + refract_color * hit.material.albedo[3]
     } else {
         background_color
     }
@@ -170,4 +185,28 @@ fn reflect(
     surface_normal: &na::Vector3<f32>,
 ) -> na::Vector3<f32> {
     2. * surface_to_light_dir.dot(surface_normal) * surface_normal - surface_to_light_dir
+}
+
+// Snell's law
+fn refract(
+    light_to_surface_dir: &na::Vector3<f32>,
+    mut plane_normal: na::Vector3<f32>,
+    mut refractive_index: f32,
+) -> na::Vector3<f32> {
+    let mut cosi = -na::clamp(plane_normal.dot(light_to_surface_dir), -1., 1.);
+    let mut etai = 1.;
+    if cosi < 0. {
+        cosi = -cosi;
+        std::mem::swap(&mut etai, &mut refractive_index);
+        plane_normal = -plane_normal;
+    }
+
+    let eta = etai / refractive_index;
+    let k = 1. - eta.powi(2) * (1. - cosi.powi(2));
+
+    if k < 0. {
+        na::Vector3::zeros()
+    } else {
+        eta * light_to_surface_dir + (eta * cosi - k.sqrt()) * plane_normal
+    }
 }
--- src/material.rs
+++ src/material.rs
@@ -2,21 +2,24 @@ use nalgebra as na;
 
 #[derive(Clone, Copy)]
 pub struct Material {
-    pub albedo: na::Vector3<f32>,
+    pub albedo: na::Vector4<f32>,
     pub diffuse_color: na::Vector3<f32>,
     pub specular_exponent: f32,
+    pub refractive_index: f32,
 }
 
 impl Material {
     pub fn new(
-        albedo: na::Vector3<f32>,
+        albedo: na::Vector4<f32>,
         diffuse_color: na::Vector3<f32>,
         specular_exponent: f32,
+        refractive_index: f32,
     ) -> Material {
         Material {
             albedo,
             diffuse_color,
             specular_exponent,
+            refractive_index,
         }
     }
 }
